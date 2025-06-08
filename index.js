fe63832b2497a664bcd8f6b77988780e86ce4f38
NadetDev <nadetdev@gmail.com>
2025-06-09 01:26:32 +0200

    add schemas: Category + Product

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+require('dotenv').config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.get('/', async (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
